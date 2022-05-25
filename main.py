d67389113e991a69a4b74286eb537bf55967640e
AuXy666 <awais.shahid_143@yahoo.com>
2022-05-25 15:16:05 +0500

    random test

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import random
+
 import read
 from datetime import date, timedelta
 slot_date = date(2022, 6, 6)
 slot = {}
 # slot["1"]=["12/05/2021","1","Zain Iqbal"]
 # slot["2"]=["13/05/2021","2","Uzair Iqbal"]
+time = [[9, 12], [2, 5]]
+
+def rGen():
+    date_rand = random.range(1, 4)
+    room_rand = random.randrange(0, read.rooms)
+    teacher_rand = random.randint(0, read.teachers)
+    time_rand = random.randint(0, 1)
+
+    print( date_rand, room_rand, teacher_rand, time_rand)
 
 # count students in a course
 
@@ -13,11 +24,17 @@ for x in read.courses:
     count = int(0)
     for y in read.studentCourse:
         print(y)
-        if y[2]==x[0]:
-            count+=1
-    countCourseStudents[x[0]]=count
+        if y[2] == x[0]:
+            count += 1
+    countCourseStudents[x[0]] = count
 
 print(countCourseStudents)
+print()
+rGen()
+#n1, n2, n3, n4 = rGen()
+#print(n1, n2, n3, n4)
+
+
 
 
 
